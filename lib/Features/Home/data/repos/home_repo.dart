9f9e3fab6cf7d94b29b28e2eb4b25a7372cd8ae9
Marco-Nagy <>
2023-12-14 08:31:13 +0200

    implements fetchNewestBooks

--- lib/Features/Home/data/repos/home_repo.dart
+++ lib/Features/Home/data/repos/home_repo.dart
@@ -3,7 +3,7 @@ import 'package:bookly/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, BookModel>> fetchNewestBooks();
 
-  Future<Either<Failure, List<BookModel>>>  fetchFeaturedBooks();
+  Future<Either<Failure, BookModel>>  fetchFeaturedBooks();
 }
---
+++ lib/Features/Home/data/repos/home_repo_impl.dart
@@ -0,0 +1,31 @@
+import 'package:bookly/Features/Home/data/models/books_model/book_model.dart';
+import 'package:bookly/Features/Home/data/repos/home_repo.dart';
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
+  @override
+  Future<Either<Failure, BookModel>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &subject=Programming &q=computer science');
+      BookModel bookModel = const BookModel();
+      bookModel = BookModel.fromJson(data);
+      return right(bookModel);
+    }  catch (error) {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, BookModel>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
